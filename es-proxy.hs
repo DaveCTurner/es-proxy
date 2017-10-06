3f3217b3b0b8e544c6419b62eab0163402c99994
David Turner <david.turner@elastic.co>
2017-10-06 11:41:34 +0100

    2-way proxy

--- es-proxy.hs
+++ es-proxy.hs
@@ -11,8 +11,10 @@ import Options.Applicative
 import Data.Monoid
 
 data Config = Config
-  { listenPort :: Int
-  , upstreamPort :: Int
+  { listenPort1   :: Int
+  , upstreamPort1 :: Int
+  , listenPort2   :: Int
+  , upstreamPort2 :: Int
   } deriving (Show, Eq)
 
 configInfo :: ParserInfo Config
@@ -20,21 +22,30 @@ configInfo = info config mempty
 
 config :: Parser Config
 config = Config
-  <$> option auto (long "listen-port")
-  <*> option auto (long "upstream-port")
+  <$> option auto (long "listen-port-1")
+  <*> option auto (long "upstream-port-1")
+  <*> option auto (long "listen-port-2")
+  <*> option auto (long "upstream-port-2")
 
 main :: IO ()
 main = execParser configInfo >>= \Config{..} -> do
-  putStrLn $ "Proxying " <> show listenPort <> " to upstream " <> show upstreamPort
 
-  runTCPServer (serverSettings listenPort "*") $ \serverAppData ->
-    runTCPClient (clientSettings upstreamPort "127.0.0.1") $ \clientAppData -> do
-      putStrLn "Opened connection"
-      let serverToClient = runConduit
-             $  appSource serverAppData
-            =$= appSink   clientAppData
-          clientToServer = runConduit
-             $  appSource clientAppData
-            =$= appSink   serverAppData
-      void $ concurrently serverToClient clientToServer
-      putStrLn "Closed connection"
+  let proxyHalfConnection sourceAppData sinkAppData = runConduit
+         $  appSource sourceAppData
+        =$= appSink   sinkAppData
+
+  let proxyConnection serverAppData clientAppData = void $ concurrently
+        (proxyHalfConnection serverAppData clientAppData)
+        (proxyHalfConnection clientAppData serverAppData)
+
+  let runProxy listenPort upstreamPort = do
+        putStrLn $ "Proxying " <> show listenPort <> " to upstream " <> show upstreamPort
+        runTCPServer (serverSettings listenPort "*") $ \serverAppData ->
+          runTCPClient (clientSettings upstreamPort "127.0.0.1") $ \clientAppData -> do
+            putStrLn "Opened connection"
+            proxyConnection serverAppData clientAppData
+            putStrLn "Closed connection"
+
+  void $ concurrently
+    (runProxy listenPort1 upstreamPort1)
+    (runProxy listenPort2 upstreamPort2)
